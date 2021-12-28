95a93bc8577e08e71610d19a68242ad7f1e2bd1a
Muhammad Anis <anisiqbal484@gmail.com>
2021-12-28 21:07:45 +0500

    worked on material ui icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Button  from '@material-ui/core/Button'
+import SaveIcon from '@material-ui/icons/Save'
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button onClick={()=>alert('hello')} 
+        <Button 
+        // onClick={()=>alert('hello')}
+        startIcon={<SaveIcon/>}
+        size="large" 
         variant="contained" 
         color="Secondary"> 
           Hello World
