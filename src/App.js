51042d7e20429600193b2134d5ba2bdd0a192a39
Muhammad Anis <anisiqbal484@gmail.com>
2021-12-28 17:44:41 +0500

    learn material ui button designes

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Button  from '@material-ui/core/Button'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button onClick={()=>alert('hello')} 
+        variant="contained" 
+        color="Secondary"> 
+          Hello World
+        </Button>
+        <img src={logo} className="App-logo" alt="logo" />
+       
+       
+         
+      </header>
+    </div>
+  );
+}
+
+export default App;
