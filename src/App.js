90c6e38945cc54187a1e10041d5012f281ca2813
Muhammad Anis <anisiqbal484@gmail.com>
2021-12-28 21:22:47 +0500

    worked on save or discard icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import Button  from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button 
-        // onClick={()=>alert('hello')}
-        startIcon={<SaveIcon/>}
-        size="large" 
-        variant="contained" 
-        color="Secondary"> 
-          Hello World
-        </Button>
+        <ButtonGroup>
+          <Button
+            // onClick={()=>alert('hello')}        
+            // endIcon={<SaveIcon/>}
+            startIcon={<SaveIcon />}
+            size="large"
+            variant="contained"
+            color="Primary">
+            Save 
+          </Button>
+
+          <Button
+            // onClick={()=>alert('hello')}        
+            // endIcon={<SaveIcon/>}
+            startIcon={<DeleteIcon />}
+            size="large"
+            variant="contained"
+            color="Secondary">
+            Discard
+          </Button>
+        </ButtonGroup>
+
+
         <img src={logo} className="App-logo" alt="logo" />
-       
-       
-         
+
+
+
       </header>
     </div>
   );
