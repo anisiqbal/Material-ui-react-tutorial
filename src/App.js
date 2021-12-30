c646c770a5cce3a310569ffc5f9755dd1d6033ae
Muhammad Anis <anisiqbal484@gmail.com>
2021-12-30 12:07:09 +0500

    worked on Textfield

--- src/App.js
+++ src/App.js
@@ -5,17 +5,31 @@ import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
-import  Checkbox from '@material-ui/core/Checkbox'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import TextField  from '@material-ui/core/TextField'
 
-function CheckboxExample(){
-  const [checked, setChecked] = React.useState (true)
-  return(
-      <div>
-        <Checkbox
-            checked = {checked}
-            onChange={(e)=> setChecked(e.target.checked)}
-        />
-      </div>
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true)
+  return (
+    <FormControlLabel
+      control={<Checkbox
+        checked={checked}
+
+        // hover and change color
+        icon={<SaveIcon />}
+        checkedIcon={<SaveIcon />}
+
+        onChange={(e) => setChecked(e.target.checked)}
+        inputProps={{
+          'aria-label': 'secondary Checkbox'
+
+        }}
+      />}
+      label="Testing Checkbox"
+    />
+
+
   )
 }
 
@@ -25,25 +39,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <CheckboxExample/>
-        <ButtonGroup  variant="contained"   color="Primary">
+        <TextField
+          variant="filled"
+          color="secondary"
+          type="email"
+          label="The Time"
+          placeholder="test@test.com"
+
+
+        />
+        <CheckboxExample />
+        
+        <ButtonGroup variant="contained" color="Primary">
           <Button
             // onClick={()=>alert('hello')}        
             // endIcon={<SaveIcon/>}
             // size="large"
             startIcon={<SaveIcon />}
-        >
-            Save 
+          >
+            Save
           </Button>
 
           <Button
             // onClick={()=>alert('hello')}        
             // endIcon={<SaveIcon/>}
             startIcon={<DeleteIcon />}
-            // size="large"
-            // variant="contained"
-            // color="Secondary"
-            >
+          // size="large"
+          // variant="contained"
+          // color="Secondary"
+          >
             Discard
           </Button>
         </ButtonGroup>
