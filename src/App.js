e4e4f7823419ee058a808cca960dfadc69b4e997
Muhammad Anis <anisiqbal484@gmail.com>
2021-12-30 11:16:53 +0500

    worked on checkbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
+import  Checkbox from '@material-ui/core/Checkbox'
+
+function CheckboxExample(){
+  const [checked, setChecked] = React.useState (true)
+  return(
+      <div>
+        <Checkbox
+            checked = {checked}
+            onChange={(e)=> setChecked(e.target.checked)}
+        />
+      </div>
+  )
+}
+
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ButtonGroup>
+        <CheckboxExample/>
+        <ButtonGroup  variant="contained"   color="Primary">
           <Button
             // onClick={()=>alert('hello')}        
             // endIcon={<SaveIcon/>}
+            // size="large"
             startIcon={<SaveIcon />}
-            size="large"
-            variant="contained"
-            color="Primary">
+        >
             Save 
           </Button>
 
@@ -25,9 +40,10 @@ function App() {
             // onClick={()=>alert('hello')}        
             // endIcon={<SaveIcon/>}
             startIcon={<DeleteIcon />}
-            size="large"
-            variant="contained"
-            color="Secondary">
+            // size="large"
+            // variant="contained"
+            // color="Secondary"
+            >
             Discard
           </Button>
         </ButtonGroup>
